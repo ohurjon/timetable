829dfd6b85ff1a1fca2c69b0170a52c6b91033a1
ohurjon <o3869006334@gmail.com>
2021-05-14 01:43:13 +0900

    Add Font Bold & CenterDrawing

--- src/main/kotlin/kr/ohurjon/timetable/TableCanvas.kt
+++ src/main/kotlin/kr/ohurjon/timetable/TableCanvas.kt
@@ -2,26 +2,46 @@ package kr.ohurjon.timetable
 
 import java.awt.*
 
-class TableCanvas(private val days : List<List<String>>): Canvas() {
+import java.awt.Font
+
+import java.awt.Rectangle
+
+import java.awt.Graphics
+
+
+
+
+class TableCanvas(private val days : Array<Array<String>>): Canvas() {
     override fun paint(g: Graphics?) {
         super.paint(g)
 
         val data = TimeWeek()
         val g2 = g as Graphics2D
 
-        g2.font = Font("NanumBarunGothic",0,30)
+        val font = Font("NanumBarunGothic",1,30)
+        g2.font = font
 
         g2.color = Color.BLACK
 
         days.forEach { day ->
             day.forEach { time ->
                 run {
-                    g2.drawString(time, data.x, data.y)
+                    drawCenteredString(g2,time,Rectangle(data.x,data.y,0,0),font)
                     data.y += data.toy
                 }
             }
             data.x += data.tox
             data.y -= data.toy*day.size
         }
+    }
+    fun drawCenteredString(g: Graphics, text: String, rect: Rectangle, font: Font) {
+        val metrics = g.getFontMetrics(font)
+
+        val x = rect.x + (rect.width - metrics.stringWidth(text)) / 2
+
+        val y = rect.y + (rect.height - metrics.height) / 2 + metrics.ascent
+        g.font = font
+
+        g.drawString(text, x, y)
     }
 }
