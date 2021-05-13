8aa187b435a7a4470a909c990f175492ef327ce1
ohurjon <o3869006334@gmail.com>
2021-05-14 01:41:46 +0900

    Center Drwaing & StringArray

--- src/main/kotlin/kr/ohurjon/timetable/TimeWeek.kt
+++ src/main/kotlin/kr/ohurjon/timetable/TimeWeek.kt
@@ -2,18 +2,18 @@ package kr.ohurjon.timetable
 
 class TimeWeek() {
 
-    val mon = listOf("월","물리","일본","화학","수학","물리","진로")
-    val tue = listOf("화","수학","문학","화학","영어","일본","생명","생명")
-    val wed = listOf("수","영어","수학","스생","물리","문학","화학","미술")
-    val thu = listOf("목","논리","논리","공학","공학","영어","문학","미술")
-    val fri = listOf("금","생물","진로","문학","수학","영어","동아리","동아리")
+    val mon = arrayOf("월","물리","일본","화학","수학","물리","진로")
+    val tue = arrayOf("화","수학","문학","화학")
+    val wed = arrayOf("수","영어","수학","스생","물리")
+    val thu = arrayOf("목","논리","논리","공학","공학","영어")
+    val fri = arrayOf("금","생물","진로","문학","수학","영어","동아리")
 
-    val sizes = listOf(mon.size,tue.size,wed.size,thu.size,fri.size)
-   // val times = stringlist(max(sizes)) //TODO
-    var days = listOf(mon,tue,wed,thu,fri)
+    val sizes = arrayOf(mon.size,tue.size,wed.size,thu.size,fri.size)
+    val times = stringArray(max())
+    var days = arrayOf(times,mon,tue,wed,thu,fri)
 
-    var x = 30
-    var y = 60
+    var x = 50
+    var y = 40
 
     val tox = 100
     val toy = 70
@@ -26,15 +26,17 @@ class TimeWeek() {
                 max = i
             }
         }
-        return max
+        return max-1
     }
 
-//    fun stringlist(value : Int) : List<String> {
-//
-//        val stringList : List<String> = mutableListOf()
-//
-//        for (i in 0..value){
-//
-//        }
-//    }            //TODO Int to String
+    fun stringArray(value : Int) : Array<String> {
+
+        val array : Array<String> = Array(value+1,{""})
+
+        for (i in 1..value){
+            array[i] = i.toString()
+        }
+
+        return array
+    }
 }
