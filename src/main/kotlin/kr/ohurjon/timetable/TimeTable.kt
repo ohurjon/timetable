52214b6bc01e63a67b2658fd2c592d5966907f41
ohurjon <o3869006334@gmail.com>
2021-05-14 01:42:40 +0900

    Add to x,y variable

--- src/main/kotlin/kr/ohurjon/timetable/TimeTable.kt
+++ src/main/kotlin/kr/ohurjon/timetable/TimeTable.kt
@@ -1,17 +1,15 @@
 package kr.ohurjon.timetable
 
-import java.awt.*
-import java.awt.List
-
-fun main(args : Array<String>) {
+fun main() {
 
     val data = TimeWeek()
 
     val canvas = TableCanvas(data.days)
 
-    val frame = TableFrame(80*6 + 30 + 60,(data.max()+1) * 70)
-
+    val frame = TableFrame((data.tox * data.days.size+1)+data.x,(data.max()+2) * data.toy)
 
     frame.add(canvas)
+
+    print(data.max())
 
 }
